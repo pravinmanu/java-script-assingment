3593936adfcde6d3de0a4d7a5bf85e3480b439da
pravin kumar mishra <110625470+pravinmanu@users.noreply.github.com>
2022-11-15 21:56:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("heding").innerHTML = "JavaScript"
+document.getElementById("p1").innerHTML = "JavaScript is a powerful, flexible, and fast programming language."
+document.getElementById("p1").style.color = 'red'
+document.getElementById("p2").innerHTML = "JavaScript powers the dynamic behavior on websites."
+document.getElementById("p2").style.color = "blue"
+document.getElementById("p3").innerHTML = "JavaScript remains at the core of web development."
+document.getElementById("p3").style.color = "green"
+document.body.style.backgroundColor = "lightpink"
